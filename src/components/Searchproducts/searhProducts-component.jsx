0e14c2f5739cd8574b6bcfbc28bafa249051dc40
Naresh141427 <a.nareshindian@example.com>
2025-11-07 11:00:03 +0530

    added full searchproducts functionalities

--- src/components/Searchproducts/searhProducts-component.jsx
+++ src/components/Searchproducts/searhProducts-component.jsx
@@ -8,20 +8,31 @@ import {
 
 const SearchProducts = ({ handleSearchproducts, currentSearch }) => {
     const [searchProduct, setSearchedProduct] = useState("")
+    const [isFocused, setIsFocused] = useState(false)
     const inputRef = useRef()
 
     useEffect(() => {
-        setSearchedProduct(currentSearch)
-    }, [currentSearch])
+        if (!isFocused) setSearchedProduct(currentSearch)
+    }, [currentSearch, isFocused])
 
     const handleOnSearch = (e) => {
         const productName = e.target.value
         setSearchedProduct(productName)
+        if (productName === "") handleSearchproducts("")
     }
     const handleForm = (e) => {
         e.preventDefault()
         handleSearchproducts(searchProduct)
         if (inputRef.current) inputRef.current.blur()
+    }
+    const handleFocus = () => {
+        setIsFocused(true)
+        setSearchedProduct("")
+    }
+    const handleBlur = () => {
+        setIsFocused(false)
+        if (searchProduct === "") setSearchedProduct(currentSearch)
+
     }
     return (
         <SearchContainer
@@ -33,6 +44,8 @@ const SearchProducts = ({ handleSearchproducts, currentSearch }) => {
                 value={searchProduct}
                 ref={inputRef}
                 onChange={handleOnSearch}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
             />
             <SearchIconButton type="submit" >
                 <SearchIcon />
